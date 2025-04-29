f9b05870b2e8fdd821ef31a35870e57ae49c4cab
JuampiBolanio <juampi360786@gmail.com>
2025-04-29 11:13:32 -0300

    Se hizo la configuración inicial del proyecto

---
+++ app/(auth)/index.tsx
@@ -0,0 +1,21 @@
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
+import React from 'react'
+
+const Auth = () => {
+    return (
+        <>
+        <SafeAreaView style={styles.container}> 
+            
+        </SafeAreaView>
+        </>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#0E3549"
+    }
+});
+
+export default Auth;
